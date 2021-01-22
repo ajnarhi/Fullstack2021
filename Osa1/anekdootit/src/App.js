9e66311f2afc247245e1ddbc53807e2ef1741857
Annukka Närhi <annukka.narhi@gmail.com>
2021-01-22 12:37:00 +0200

    1.13

--- Osa1/anekdootit/src/App.js
+++ Osa1/anekdootit/src/App.js
@@ -16,17 +16,30 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
+  const [votes, setVotes] = useState ([0, 0, 0, 0, 0, 0]) 
    
+
   const [selected, setSelected] = useState(0)
   const randomSelected = () =>{
     setSelected (Math.floor(Math.random() * anecdotes.length))
   }
   console.log(selected)
+
+  const voteGiven= () => {
+    const copy = [...votes] //tekee taulukon, jossa on kaikki elementit votes taulukosta
+     copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>This anekdote has this many votes: {votes[selected]}</p>
+      <Button  handleClick={voteGiven}
+        text='Vote this anekdote'/>
       <Button  handleClick={randomSelected}
         text='Next anecdote!'/>
+      
     </div>
   )
 }
