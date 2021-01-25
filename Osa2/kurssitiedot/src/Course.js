1fdc66cabb75e61f52dbd89c8a31f9313f47e437
Annukka Närhi <annukka.narhi@gmail.com>
2021-01-25 11:24:20 +0200

    2.1, 2.2, 2.4, 2.5. Missing 2.3 still. Working on it

---
+++ Osa2/kurssitiedot/src/Course.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+
+  console.log('coursen propsit', course)
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log('content propsit', props)
+  return (
+    <div>
+      <span>
+        {props.parts.map(part =>
+          <Part key={part.id} name={part.name} exercise={part.exercises} />
+        )}
+      </span>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log('yhteenlaskettu määrä', props)
+
+  const total = props.parts.reduce( (s, p) => {
+    console.log('what is happening', s, p)
+    return null 
+  })
+  return (
+    <div>
+      <p>
+        Number of exercises {total}
+      </p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+
+  return (
+    <div>
+      <p>
+        {props.name} {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+export default Course
