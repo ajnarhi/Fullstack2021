0179ccda6f056f4fdb0d2dd77cde4e41111ab0ba
Annukka Närhi <annukka.narhi@gmail.com>
2021-02-01 10:08:45 +0200

    2.18 with fixed lines

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -10,13 +10,13 @@ const Filter = (props) => {
     props.setFilteredName(event.target.value)
   }
 
-  
+
 
   return (
     <form>
       <div>
-        Filter: <input value={props.filteredName} 
-        onChange={handleFilterChange} />
+        Filter: <input value={props.filteredName}
+          onChange={handleFilterChange} />
       </div>
     </form>
   )
@@ -31,16 +31,17 @@ const PersonForm = (props) => {
     event.preventDefault()
     console.log('button clicked', event.target)
     //props.persons.some(p => p.name === props.newName some kertoo, että löytyykö vai eikö löydy (true, false)
-    const existingPerson=props.persons.filter(p => p.name === props.newName) //filter tekee listan löytyneistä/löytyneestä
-    if (existingPerson.length>0) {
+    const existingPerson = props.persons.filter(p => p.name === props.newName) //filter tekee listan löytyneistä/löytyneestä
+    if (existingPerson.length > 0) {
       window.confirm(props.newName + ' is already added to phonebook. Do you want to replace their old number with the new number?')
       personService
         .replaceOldNumber(existingPerson[0].id, props.newName, props.newNumber)
         .then(response => {
-      props.setNewName('')
-      props.setNewNumber('')
-      props.setPersons(props.persons.filter(n => n.id !== response.data.id).concat(response.data))
-    } )}else {
+          props.setNewName('')
+          props.setNewNumber('')
+          props.setPersons(props.persons.filter(n => n.id !== response.data.id).concat(response.data))
+        })
+    } else {
 
       const personObject = {
         name: props.newName,
@@ -48,14 +49,14 @@ const PersonForm = (props) => {
       }
 
       personService.create(personObject)
-    .then(response => {
-      console.log(response)
-      props.setPersons(props.persons.concat(response.data))
-      props.setNewName('')
-      props.setNewNumber('')
-    })
+        .then(response => {
+          console.log(response)
+          props.setPersons(props.persons.concat(response.data))
+          props.setNewName('')
+          props.setNewNumber('')
+        })
 
-     
+
     }
   }
 
@@ -96,38 +97,38 @@ const Persons = (props) => {
   }
 
   const DeleteButton = (props) => {
-    return(
+    return (
 
-    <button onClick={()=>deleteThisPerson(props.person.id, props.person.name)}>Delete</button>
+      <button onClick={() => deleteThisPerson(props.person.id, props.person.name)}>Delete</button>
     )
   }
 
-  if (props.filteredName===''){
-  return (
-    <div>
-      {props.persons.map(person =>
-        <li key={person.name}> 
-          {person.name} {person.number}  {} 
-        
-        <DeleteButton person={person}/>
-        </li>
-      )
-      }</div>
+  if (props.filteredName === '') {
+    return (
+      <div>
+        {props.persons.map(person =>
+          <li key={person.name}>
+            {person.name} {person.number}  { }
 
-  )
-}else{
-  return(
-    <div>
-  {props.persons.filter(person => person.name.includes(props.filteredName)).map(person =>
-        <li key={person.name}>
-          {person.name} {person.number} {} 
-        
-        <button onClick={()=>deleteThisPerson(person.id, person.name)}>Delete</button>
-        </li>
+            <DeleteButton person={person} />
+          </li>
+        )
+        }</div>
 
-      )}
-  </div>)
-}
+    )
+  } else {
+    return (
+      <div>
+        {props.persons.filter(person => person.name.includes(props.filteredName)).map(person =>
+          <li key={person.name}>
+            {person.name} {person.number} { }
+
+            <button onClick={() => deleteThisPerson(person.id, person.name)}>Delete</button>
+          </li>
+
+        )}
+      </div>)
+  }
 
 }
 
@@ -138,7 +139,7 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filteredName, setFilteredName]=useState ('')
+  const [filteredName, setFilteredName] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -167,7 +168,7 @@ const App = () => {
       <h2>Phonebook</h2>
 
       <h2>Filter phonebook</h2>
-      <Filter persons={persons} filteredName={filteredName} setFilteredName={setFilteredName}/>
+      <Filter persons={persons} filteredName={filteredName} setFilteredName={setFilteredName} />
 
       <h2>Add new person to phonebook</h2>
 
@@ -179,7 +180,7 @@ const App = () => {
       <Persons persons={persons} filteredName={filteredName} setPersons={setPersons} />
     </div>
   )
- 
+
 
 }
 
