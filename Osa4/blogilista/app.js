9f7e197e644335ca12215ba6e1411c856d8e5fbc
Annukka Närhi <annukka.narhi@gmail.com>
2021-02-15 16:14:36 +0200

    4.18 finished, token working

--- Osa4/blogilista/app.js
+++ Osa4/blogilista/app.js
@@ -6,6 +6,11 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const loginRouter = require('./controllers/login')
+
+
+
+
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
@@ -15,6 +20,7 @@ const blogsRouter = require('./controllers/blogs')
 app.use('/api/blogs', blogsRouter)
 const usersRouter = require('./controllers/users')
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 module.exports = app
