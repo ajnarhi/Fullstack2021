d295ecd977f8e089401623a89857d527edb0007d
Annukka Närhi <annukka.narhi@gmail.com>
2021-02-20 16:17:05 +0200

    5.1 finished

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage]=useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +16,61 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  if (user === null) {
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          Username:
+          <p> </p>
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+        <p></p>
+          Password:
+          <p></p>
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <p> </p>
+        <button type="submit">Log in</button>
+      </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      {user.username} logged in!
+      <p> </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
