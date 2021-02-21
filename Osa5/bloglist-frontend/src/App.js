bf91f8a237238439c7da20658b6e98a3f0b567ff
Annukka Närhi <annukka.narhi@gmail.com>
2021-02-21 10:57:03 +0200

    5.4

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -4,12 +4,32 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const Notification = ({ message, isError }) => {
+  if (message === null) {
+    return null
+  }else if (isError){
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}else{
+  return(
+    <div className="positiveinfo">
+      {message}
+    </div>
+  )
+  }
+}
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage]=useState(null)
+  const [positiveMessage, setPositiveMessage]=useState(null)
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
@@ -67,19 +87,25 @@ const App = () => {
     event.preventDefault()
     console.log('clicked')
     const newBlog= {title,author,url}
-    blogService.create(newBlog)
-    .then (returnedBlog=>{
+    blogService.create(newBlog) //tämä kohta rikkoi hetkellisesti sisäänkirjautumisen. Miksi?
+    .then (returnedBlog=>{ //luodaan uusi blogi ja luodaan uusi lista johon lisätään palautettu blogi
       setBlogs(blogs.concat(returnedBlog))
     })
+    setPositiveMessage(title + ' added to bloglist')
+      setTimeout(() => {
+        setPositiveMessage(null)
+      }, 5000)
     setAuthor('')
     setTitle('')
     setUrl('')
-  }
+  } 
 
   if (user === null) {
   return (
     <div>
       <h2>Log in to application</h2>
+      <Notification message={errorMessage} isError={true} />
+
       <form onSubmit={handleLogin}>
         <div>
           Username:
@@ -115,11 +141,8 @@ const App = () => {
       {user.name} logged in!
       <p> </p>
       <button onClick={handleLogout}>Log out!</button>
-      <p> </p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
 <h2>Create new blog</h2>
+<Notification message={positiveMessage} isError={false}/>
 <p> </p>
 <form onSubmit={handleNewblog}>
         <div>
@@ -157,6 +180,11 @@ const App = () => {
         <p> </p>
         <button type="submit">Create blog</button>
       </form>
+      
+      <p> </p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
     </div>
   )
 }
