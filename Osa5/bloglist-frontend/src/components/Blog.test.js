b4d36b00e2be698db3b1c2826999c787f230b738
Annukka Närhi <annukka.narhi@gmail.com>
2021-02-22 09:17:10 +0200

    5.14

--- Osa5/bloglist-frontend/src/components/Blog.test.js
+++ Osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
-import { prettyDOM } from '@testing-library/dom' 
+import { prettyDOM } from '@testing-library/dom'
 //import blog from '../../../blogapp-backend/models/blog'
 
 
-test('renders content', () => {
+test('renders content without author, url or likes', () => {
   const blog = {
     title: 'Test blog',
     author: 'Test author',
@@ -23,4 +23,28 @@ test('renders content', () => {
   expect(url).not.toBeVisible()
   const likes=component.container.querySelector('.likes')
   expect(likes).not.toBeVisible()
+})
+
+test('renders content with author, url or likes when button clicked', () => {
+  const blog = {
+    title: 'Test blog',
+    author: 'Test author',
+    url: 'Test url',
+    likes: '120'
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('View bloginfo')
+  fireEvent.click(button)
+
+
+  const author=component.getByText('Test author')
+  expect(author).toBeVisible()
+  const url=component.getByText('Test author')
+  expect(url).toBeVisible()
+  const likes=component.container.querySelector('.likes')
+  expect(likes).toBeVisible()
 })
