264e172d2eac564eb3c6eb421a22039ec7316e03
Annukka Närhi <annukka.narhi@gmail.com>
2021-02-23 10:51:44 +0200

    6.2 done

--- Osa6/unicafe-redux/src/index.js
+++ Osa6/unicafe-redux/src/index.js
@@ -5,7 +5,59 @@ import reducer from './reducer'
 
 const store = createStore(reducer)
 
+const Statistics = (props) => {
+
+  const allFeedback= (props.goodValue+props.neutralValue+props.badValue)
+  const averageFeedback = (props.goodValue*1 + props.badValue * (-1)) / allFeedback
+  const goodPercentageFeedback = ((props.goodValue)/allFeedback)*100
+
+  if (props.goodValue===0 && props.neutralValue===0 && props.badValue===0){
+    return(
+    <div>
+      <h2>{props.statistics}</h2>
+      <p>No feedback given!</p></div>
+    )
+  }
+  return  (
+
+   
+    
+    <div>
+
+<h2>{props.statistics}</h2>
+    
+    <table>
+      <tbody>
+    <StatisticLine text="Good" value={props.goodValue}/>
+  
+    <StatisticLine text="Neutral" value={props.neutralValue}/>
+    
+    <StatisticLine text="Bad" value={props.badValue}/>
+  
+    <StatisticLine text="All" value={allFeedback}/>
+    
+    <StatisticLine text="Average" value={averageFeedback}/>
+   
+    <StatisticLine text="Positive feedback given" value={goodPercentageFeedback + "%"} />
+
+    </tbody>
+    </table>
+    </div>
+
+  )
+}
+
+const StatisticLine=(props)=> {
+  return (
+    <tr>
+    <td>{props.text}</td> <td>{props.value}</td>
+    </tr>
+  )
+}
+
+
 const App = () => {
+  const statistics = 'Statistics'
   const good = () => {
     store.dispatch({
       type: 'GOOD'
@@ -38,6 +90,7 @@ const App = () => {
       <div>good {store.getState().good}</div>
       <div>neutral {store.getState().ok}</div>
       <div>bad {store.getState().bad}</div>
+      <Statistics statistics={statistics} goodValue={store.getState().good} neutralValue={store.getState().ok} badValue={store.getState().bad}/>
     </div>
   )
 }
